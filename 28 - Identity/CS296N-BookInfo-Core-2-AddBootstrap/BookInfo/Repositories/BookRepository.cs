183ee5aea58ccdeadd232baa6ed447a5db4fe36e
Tim Allen <carver0428@hotmail.com>
2019-01-15 21:18:42 -0800

    CS296 First Commit
    This is the first commit of the term

---
+++ 28 - Identity/CS296N-BookInfo-Core-2-AddBootstrap/BookInfo/Repositories/BookRepository.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BookInfo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookInfo.Repositories
+{
+    // The repository object will be injected into a Controller by a transient service (configured in Startup)
+    public class BookRepository : IBookRepository
+    {
+        private readonly ApplicationDbContext context;
+ 
+        public BookRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+        
+        /* Interface method implementations */
+
+        public List<Book> GetAllBooks()
+        {
+            return context.Books.Include(b => b.Authors).ToList();
+        }
+
+        public Book GetBookByTitle(string title)
+        {
+            return context.Books.Include(b => b.Authors).First(b => b.Title == title);
+        }
+
+        public Book GetBookById(int id)
+        {
+            return context.Books.Include("Authors").First(b => b.BookID == id);
+        }
+
+        public List<Book> GetBooksByAuthor(Author author)
+        {
+            return (from b in context.Books
+                    where b.Authors.Contains(author)
+                   select b).Include(b => b.Authors).ToList();
+        }
+
+        public List<Author> GetAuthorsByBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddBook(Book book)
+        {
+            // Add the book to the database
+            context.Books.Update(book);
+            // Save the book so that it gets an ID (primary key value)
+            context.SaveChanges();
+
+            // Give each author object a FK for the book
+            // and add it to the database
+            foreach (Author a in book.Authors)
+            {
+                a.BookID = book.BookID;
+                context.Authors.Update(a);
+            }
+            
+            return context.SaveChanges();
+        }
+        
+        public int Edit(Book book)
+        {
+            context.Books.Update(book);
+            return context.SaveChanges();
+        }
+
+        public int Delete(int id)
+        {
+            var bookFromDb = context.Books.First(a => a.BookID == id);
+            context.Remove(bookFromDb);
+            return context.SaveChanges();
+        }
+
+        // TODO: implement this method
+        /*
+        public List<Author> GetAuthorsByBook(Book book)
+        {
+            return context.Books.Authors;
+        }
+        */
+    }
+}
---
+++ 28 - Identity/CS296N-BookInfo-Core-2-AddBootstrap/BookInfo/Repositories/IBookRepository.cs
@@ -0,0 +1,21 @@
+﻿using BookInfo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace BookInfo.Repositories
+{
+    public interface IBookRepository
+    {
+        List<Book> GetAllBooks();
+        Book GetBookByTitle(string title);
+        Book GetBookById(int id);
+        List<Book> GetBooksByAuthor(Author author);
+        List<Author> GetAuthorsByBook(Book book);
+        int AddBook(Book book);
+        int Edit(Book book);
+        int Delete(int id);
+    }
+}
