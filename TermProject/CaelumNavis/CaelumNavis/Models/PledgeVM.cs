6064c4b942295ad86011f5c86280e39d3589d809
Tim Allen <carver0428@hotmail.com>
2019-01-27 23:20:53 -0800

    This is the production version of lab 1. Identity has been added and form validation but only in page not in identity.

---
+++ TermProject/CaelumNavis/CaelumNavis/Models/PledgeVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaelumNavis.Models
+{
+	public class PledgeVM
+	{
+		List<Pledge> Pledges { get; set; }
+		Pledge Pledge { get; set; }
+
+	}
+}
