a3c0dd21c819ea6c18cf01898a0e669154986bd8
Tim Allen <carver0428@hotmail.com>
2019-01-31 12:59:41 -0800

    This is a work save commit. Login Controller logic was added as well as the basic Role controller class along with modifications to Customer VM and multiple views

---
+++ Week2/TermProject/CaelumNavis/CaelumNavis/Controllers/RoleAdminController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using CaelumNavis.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaelumNavis.Controllers
+{
+
+	[Authorize(Roles = "Admins")]
+	public class RoleAdminController : Controller
+	{
+		private RoleManager<IdentityRole> roleManager;
+		private UserManager<Customer> userManager;
+
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public RoleAdminController(RoleManager<IdentityRole> roleMgr,
+								   UserManager<Customer> userMrg)
+		{
+			roleManager = roleMgr;
+			userManager = userMrg;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create([Required]string name)
+		{
+			if (ModelState.IsValid)
+			{
+				IdentityResult result
+					= await roleManager.CreateAsync(new IdentityRole(name));
+				if (result.Succeeded)
+				{
+					return RedirectToAction("Index");
+				}
+				else
+				{
+					AddErrorsFromResult(result);
+				}
+			}
+			return View(name);
+		}
+		private void AddErrorsFromResult(IdentityResult result)
+		{
+			foreach (IdentityError error in result.Errors)
+			{
+				ModelState.AddModelError("", error.Description);
+			}
+		}
+	}
+}
---
+++ Week2/TermProject/CaelumNavis/CaelumNavis/Models/Repos/AppDBContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CaelumNavis.Models.Repos
+{
+	public class AppDBContext : IdentityDbContext
+	{
+		public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+		public DbSet<Message> Messages { get; set; }
+		public DbSet<Customer> Customers { get; set; }
+		public static async Task CreateAdminAccount(IServiceProvider serviceProvider,
+		IConfiguration configuration)
+		{
+			
+			string username = configuration["Data:AdminUser:Name"];
+			string email = configuration["Data:AdminUser:Email"];
+			string password = configuration["Data:AdminUser:Password"];
+			string role = configuration["Data:AdminUser:Role"];
+
+			const string ADMIN = "Admin";
+			const string MEMBER = "Member";
+
+			
+
+			Customer user = new Customer
+			{
+			
+			UserName = username,
+				Email = email
+			};
+
+		
+		}
+	}
+}
