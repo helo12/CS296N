bc59efa482dacf149a6bc1bf6ed18111f374b9d1
Tim Allen <carver0428@hotmail.com>
2019-02-06 14:09:44 -0800

    stupid thing wont recognize file changes

--- README.md
+++
@@ -1,2 +0,0 @@
-# CS296N
-This is the repo for the class CS 296N. This is a class on ASP.NET. We are using ASP.NET CORE 2.0 and possibly 2.1
--- Week1/CS296N-AddingIdentity/29Applying Identity/Users/Users/Models/AppUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Users.Models {
-
-    public class AppUser : IdentityUser {
-        // no additional members are required
-        // for basic Identity installation
-    }
-}
